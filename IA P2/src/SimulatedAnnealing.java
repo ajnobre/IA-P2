16e2814b7867cc08a2f57e06d59cbf9f58b31c16
alvaro <alvaro@alvaro-SATELLITE-L855>
2018-10-13 17:51:25 +0100

    v0.3 distance variation

--- IA P2/src/SimulatedAnnealing.java
+++ IA P2/src/SimulatedAnnealing.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class SimulatedAnnealing {
@@ -11,12 +12,13 @@ public class SimulatedAnnealing {
 	static File file;
 	static Scanner scanner;
 	static List<String> cities = new ArrayList<String>();
+//	static List<Integer> path = new ArrayList<Integer>();
 	static int[][] matrix;
 
 	public static void main(String[] args) throws IOException {
 		switch (args.length) {
 		case 2:
-			fileDir = "/home/alvaro/eclipse-workspace/IA P2/" + args[0];
+			fileDir = "/home/alvaro/git/repository/IA P2/" + args[0];
 			file = new File(fileDir);
 			scanner = new Scanner(file);
 			String line = "";
@@ -33,23 +35,9 @@ public class SimulatedAnnealing {
 
 			String[] tokens = null;
 
-			for (int i = 0; scanner.hasNextLine(); i++) {
-				line = scanner.nextLine();
-				tokens = line.split("\\s+");
-
-				if (i == 0) {
-					cities.add(tokens[1]);
-				} else {
-					cities.add(tokens[0]);
-					for (int j = 0; j + 1 < tokens.length - 1; j++) {
-						matrix[i][j] = Integer.parseInt(tokens[j + 1]);
-						matrix[j][i] = Integer.parseInt(tokens[j + 1]);
-					}
-				}
-				matrix[i][i] = 0;
-			}
+			makeMatrix();
 			// Lê o percurso
-			fileDir = "/home/alvaro/eclipse-workspace/IA P2/" + args[1];
+			fileDir = "/home/alvaro/git/repository/IA P2/" + args[1];
 			file = new File(fileDir);
 			scanner = new Scanner(file);
 			tokens = scanner.nextLine().split("\\s+");
@@ -65,7 +53,9 @@ public class SimulatedAnnealing {
 
 			// Calcular custo
 
-			System.out.println(distance(path));
+			for (int i = 0; i < 10; i++) {
+				System.out.println(distanceVariation(path, extractIJ(path)));
+			}
 			break;
 		default:
 			System.out.printf("filename");
@@ -73,12 +63,76 @@ public class SimulatedAnnealing {
 		}
 	}
 
+	private static void makeMatrix() {
+		String line;
+		String[] tokens;
+		for (int i = 0; scanner.hasNextLine(); i++) {
+			line = scanner.nextLine();
+			tokens = line.split("\\s+");
+
+			if (i == 0) {
+				cities.add(tokens[1]);
+			} else {
+				cities.add(tokens[0]);
+				for (int j = 0; j + 1 < tokens.length - 1; j++) {
+					matrix[i][j] = Integer.parseInt(tokens[j + 1]);
+					matrix[j][i] = Integer.parseInt(tokens[j + 1]);
+				}
+			}
+			matrix[i][i] = 0;
+		}
+	}
+
+	public static int[] extractIJ(int[] path) {
+		Random rand = new Random();
+		int[] ij = new int[2];
+
+		int i = rand.nextInt(path.length);
+
+		int[] neighboors = getNeighboors(path.length - 1, i);
+		int j = rand.nextInt(path.length);
+
+		while (j == i || j == (neighboors[0]) || j == (neighboors[1])) {
+			j = rand.nextInt(path.length);
+		}
+		ij[0] = i;
+		ij[1] = j;
+
+		return ij;
+	}
+
+	private static int[] getNeighboors(int length, int i) {
+		int[] neighboors = new int[2];
+		if (i == length) {
+			neighboors[0] = i - 1;
+			neighboors[1] = 0;
+		} else if (i == 0) {
+			neighboors[0] = length;
+			neighboors[1] = i + 1;
+		} else {
+			neighboors[0] = i + -1;
+			neighboors[1] = i + 1;
+		}
+		return neighboors;
+	}
+
 	public static int distance(int[] path) {
 		int cost = 0;
 		for (int i = 0; i < path.length - 1; i++) {
 			cost += matrix[path[i]][path[i + 1]];
 		}
+		// distancia do fim ao inicio
+		cost += matrix[path[0]][path[path.length - 1]];
 		return cost;
+	}
+
+	public static int distanceVariation(int[] path, int[] ij) {
+		int[] iNbrs = getNeighboors(path.length - 1, ij[0]);
+		int[] jNbrs = getNeighboors(path.length - 1, ij[1]);
+		System.out.println(iNbrs[0] + " " + iNbrs[1] + " " + jNbrs[0] + " " + jNbrs[1]);
+		return matrix[path[ij[0]]][path[ij[1]]] + matrix[path[iNbrs[1]]][path[jNbrs[1]]] - matrix[path[ij[0]]][iNbrs[1]]
+				- matrix[ij[1]][jNbrs[1]];
+
 	}
 
 }
